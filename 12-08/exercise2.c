4b60724792795a47c213d1a825def534225603b6
Sanuja Methmal <methmal66@yahoo.com>
2021-08-12 11:00:48 +0530

    exercise2

--- 12-08/exercise2.c
+++ 12-08/exercise2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(const int argc, const char const *argv[]) {
+int main(const int argc, const char *argv[]) {
   printf("This\tis\tmy\tfirst\tc\tprogram\n");
   return 0;
 }
